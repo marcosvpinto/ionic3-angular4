ca87eeac7868fe934d38a0b5459521537bf52730
Marcos Vinícius Pinto <marcos.pinto@unigranrio.com.br>
2017-08-01 14:12:33 -0300

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
+import { RestProvider } from './../../providers/rest/rest';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  countries: string[];
+  errorMessage: string;
 
+  constructor(public navCtrl: NavController, public rest: RestProvider) {
+
+  }
+
+  ionViewDidLoad(){
+    this.getCountries();
+  }
+
+  getCountries(){
+    this.rest.getCountries()
+      .subscribe(
+        countries=>this.countries = countries,
+        error=>this.errorMessage = <any>Error
+      );
   }
 
 }
